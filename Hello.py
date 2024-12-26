af22f6f9ffe8090275c4aa1f21a53425b0b47aed
RishmaZakariya <rishmazakariya@gmail.com>
2024-12-26 19:36:11 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,4 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello! One of a BSc in IT (R) OUSL student here" )
+Hello! One of a BSc in IT (R) OUSL student here
